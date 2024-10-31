1d6e278cd65b7db9f96eab5601d270ca6e1880e9
Joshua Treudler <joshua@treudler.net>
2024-10-31 21:28:30 +0100

    Update Gource resolution and visualization settings

--- gourcer.sh
+++ gourcer.sh
@@ -3,7 +3,7 @@
 # Define the Gource visualization settings
 # More settings can be set directly in the Gource command below
 title="Development Visualization"
-resolution="1536x864"
+resolution="600x338"
 output_file="gource.mp4"
 
 # Create a temporary directory within the project directory
@@ -23,7 +23,7 @@ done
 cat ${tmp_dir}/gource-* | sort -n > ${tmp_dir}/combined.txt
 
 # Generate the Gource visualization video
-gource ${tmp_dir}/combined.txt --seconds-per-day 0.3 --auto-skip-seconds 0.1 --title "$title" --disable-auto-rotate --camera-mode overview --user-friction 1 --max-user-speed 15 --filename-time 5 --highlight-users --time-scale 4 --user-scale 5 -${resolution} -o - | \
+gource ${tmp_dir}/combined.txt --seconds-per-day 5 --auto-skip-seconds 0.1 --title "$title" --disable-auto-rotate --camera-mode overview --user-friction 1 --max-user-speed 15 --filename-time 5 --highlight-users --time-scale 4 --user-scale 1.2 --hide usernames -${resolution} -o - | \
 ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 "$output_file"
 
 # Delete the custom logs
